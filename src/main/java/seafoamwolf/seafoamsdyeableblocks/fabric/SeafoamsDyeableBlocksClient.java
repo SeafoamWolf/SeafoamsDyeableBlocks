66231155f641835d7510254806c150a5a3280eb1
SeafoamWolf <124807123+SeafoamWolf@users.noreply.github.com>
2023-11-07 09:34:40 -0600

    Parity with released version

--- src/main/java/seafoamwolf/seafoamsdyeableblocks/fabric/SeafoamsDyeableBlocksClient.java
+++ src/main/java/seafoamwolf/seafoamsdyeableblocks/fabric/SeafoamsDyeableBlocksClient.java
@@ -3,15 +3,12 @@ package seafoamwolf.seafoamsdyeableblocks.fabric;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-
-import seafoamwolf.seafoamsdyeableblocks.fabric.client.block.DyedBlockClient;
-import seafoamwolf.seafoamsdyeableblocks.fabric.client.item.DyedItemClient;
+import seafoamwolf.seafoamsdyeableblocks.fabric.client.DyedColorHandler;
 
 @Environment(value=EnvType.CLIENT)
 public class SeafoamsDyeableBlocksClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		DyedBlockClient.register();
-		DyedItemClient.register();
+		DyedColorHandler.register();
 	}
 }
